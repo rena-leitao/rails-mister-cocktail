b074e1139865476f333f82162802727cab189a4f
Renata Leitao <asadebarata@gmail.com>
2018-01-23 14:08:01 -0200

    add a route pro index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-
-
+root to: 'cocktails#index'
   resources :cocktails, only: [ :index, :show, :new, :create] do
     resources :doses, only: [ :create, :new ]
   end
